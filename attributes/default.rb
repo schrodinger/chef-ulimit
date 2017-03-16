f0227b570da5506c2735d3874fb125dd1d3b0ca5
Gennadiy Nerubayev <gennadiy.nerubayev@schrodinger.com>
2017-03-16 00:33:28 -0400

    Fix the wrong conditional order..

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 default['ulimit']['pam_su_template_cookbook'] = nil
 default['ulimit']['users'] = Mash.new
-if node[:platform] == 'suse'
+if node[:platform_family] == 'suse'
   # Suse has an atypical directory for ulimit settings
-  default['ulimit']['security_limits_directory'] = '/etc/security/limits.d'
-else
   default['ulimit']['security_limits_directory'] = '/etc/security'
+else
+  default['ulimit']['security_limits_directory'] = '/etc/security/limits.d'
 end
 
 default['ulimit']['ulimit_overriding_sudo_file_name'] = 'sudo'
